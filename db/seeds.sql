72a2b35aca8f23e3730b99ef6707876051aef389
SheetalSrikumar <bs.sheetal@gmail.com>
2018-12-19 22:59:40 -0800

    added sql files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (name,devoured) VALUES ('Quinoa Burger',true);
+INSERT INTO burger (name,devoured) VALUES ('Veggie Burger',true);
+INSERT INTO burger (name,devoured) VALUES ('Paneer Burger',false);
