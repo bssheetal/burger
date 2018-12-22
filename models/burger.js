b2094408157d4fa76683c0024bd2cdb97dfaae6d
SheetalSrikumar <bs.sheetal@gmail.com>
2018-12-21 18:38:10 -0800

    PUT method working in controller

--- models/burger.js
+++ models/burger.js
@@ -14,7 +14,15 @@ var burger={
         {
             cb(res);
         });
-    }
+    },
+    
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
+      }
+
+
 }
 
 module.exports=burger;
